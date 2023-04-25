acad73e3102ee6e65833edc61945d89d6c6687ac
Bruno Frizzo <brunofrizzo@outlook.com>
2023-04-25 14:33:06 -0300

    Select de tipo de veiculo adicionado.

--- src/app.js
+++ src/app.js
@@ -1,36 +1,30 @@
 // Get the GitHub username input form
 const formFipe = document.getElementById('formFipe');
+const selectTipo = document.getElementById('selectTipo');
 const selectMarca = document.getElementById('selectMarca');
 const selectModelo = document.getElementById('selectModelo');
 const selectAno = document.getElementById('selectAno');
 const buttonSubmit = document.getElementById('buttonSubmit');
 
-fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas').then(response => response.json())
-.then(data => {
-    if (data) {
-        data.forEach(element => {
-            let option = document.createElement('option');
-            option.value = element.codigo;
-            option.innerHTML = element.nome;
-            selectMarca.appendChild(option);
-        })
-    }
-})
-
 // Listen for submissions on GitHub username input form
 formFipe.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let gitHubUsername = document.getElementById('usernameInput').value;
-    let repositoryName = document.getElementById('repositorySelect').value;
+    let tipoVeiculo = selectTipo.value;
+    let marca = selectMarca.value;
+    let modelo = selectModelo.value;
+    let ano = selectAno.value;
 
-    if (gitHubUsername && repositoryName) {
-        inputSubmit.value = 'Buscando commits...';
-        inputSubmit.disabled = true;
+    if (tipoVeiculo && marca && modelo && ano) {
+        buttonSubmit.value = 'Buscando...';
+        buttonSubmit.disabled = true;
 
-        requestRepoCommits(gitHubUsername, repositoryName)
+        buscarHistoricoPrecos(tipoVeiculo, marca, modelo, ano)
             .then(response => response.json()) // parse response into json
             .then(data => {
+                console.log(data);
+                return;
+
                 if (data) {
                     let ul = document.getElementById('repoCommits');
                     ul.innerHTML = "";
@@ -59,10 +53,42 @@ formFipe.addEventListener('submit', (e) => {
                     }
                 }
 
-                inputSubmit.disabled = false;
-                inputSubmit.value = 'Buscar';
+                buttonSubmit.disabled = false;
+                buttonSubmit.value = 'Buscar';
             })
     }
+})
+
+selectTipo.addEventListener('change', (e) => {
+    e.preventDefault();
+    selectMarca.disabled = true;
+    selectModelo.disabled = true;
+    buttonSubmit.disabled = true;
+    selectAno.innerHTML = '';
+    selectMarca.innerHTML = '';
+    selectModelo.innerHTML = '';
+
+    let tipoVeiculo = selectTipo.value;
+
+    buscarMarcas(tipoVeiculo)
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                let option = document.createElement('option');
+                option.value = '';
+                option.innerHTML = 'Selecione uma marca';
+                selectMarca.appendChild(option);
+                data.forEach(element => {
+                    let option = document.createElement('option');
+                    option.value = element.codigo;
+                    option.innerHTML = element.nome;
+                    selectMarca.appendChild(option);
+                })
+            }
+            selectMarca.disabled = false;
+            selectModelo.disabled = false;
+            buttonSubmit.disabled = false;
+        })
 })
 
 selectMarca.addEventListener('change', (e) => {
@@ -108,7 +134,6 @@ selectModelo.addEventListener('change', (e) => {
     buscarAnos(codigoMarca, codigoModelo)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             if (data) {
                 selectAno.innerHTML = '';
                 data.forEach(element => {
@@ -124,10 +149,20 @@ selectModelo.addEventListener('change', (e) => {
         })
 })
 
+
+function buscarMarcas(tipo) {
+    return Promise.resolve(fetch(`https://parallelum.com.br/fipe/api/v1/${tipo}/marcas`));
+}
+
 function buscarModelos(codigoMarca) {
     return Promise.resolve(fetch(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${codigoMarca}/modelos`));
 }
 
 function buscarAnos(codigoMarca, codigoModelo) {
     return Promise.resolve(fetch(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${codigoMarca}/modelos/${codigoModelo}/anos`));
+}
+
+function buscarHistoricoPrecos(tipo, codigoMarca, codigoModelo, ano) {
+    //GET: https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/2014-3
+    return Promise.resolve(fetch(`https://parallelum.com.br/fipe/api/v1/${tipo}/marcas/${codigoMarca}/modelos/${codigoModelo}/anos/${ano}`));
 }
